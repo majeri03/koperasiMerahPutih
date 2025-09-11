8201910ee9fc53acd3e97e48ea7b8d8235da0b23
ALVIAN BURHANI <139302290+alviansyahburhani@users.noreply.github.com>
2025-09-11 08:07:28 +0000

    landing page,login,register,dashanggota

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// frontend/app/page.tsx (File Sementara)
-
-import Home from './(publik)/page';
-
-// Komponen ini hanya untuk development, agar bisa diakses di localhost:3000
-export default function TemporaryHomePage() {
-  // Kita panggil komponen Home asli dengan parameter "dummy"
-  return <Home params={{ tenant: 'development' }} />;
-}
